9ad9709d6fa5bd2aadcbb9dd0c2a11d59d145531
James <xusemaj@gmail.com>
2024-11-02 15:33:05 +0000

    pushed classes for logic

---
+++ test_location.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov  2 13:28:30 2024
+
+@author: james
+"""
+
+from streamlit_geolocation import streamlit_geolocation
+location = streamlit_geolocation()
+print(location)
