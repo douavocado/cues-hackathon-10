142a84cbf93e35c7db29dfe61e6c174b2bfc95a8
Alexander Bulloso <155785152+AlexBulloso@users.noreply.github.com>
2024-11-02 14:50:14 +0000

    Location verification

--- components/library_selector.py
+++ components/library_selector.py
@@ -1,6 +1,9 @@
 # handle library selection and recommend new libraries?
 
 import streamlit as st
+from streamlit_geolocation import streamlit_geolocation
+
+
 
 class LibrarySelector:
     def __init__(self):
@@ -8,14 +11,59 @@ class LibrarySelector:
 
     def render(self):
         # Example list of libraries with their coordinates (latitude, longitude)
-        libraries = {
-            "Library A": (52.2053, 0.1218),
-            "Library B": (52.2040, 0.1230),
-            "Library C": (52.2030, 0.1240),
+        department_libraries_coordinates = {
+            "Department of Engineering Library": {
+                "min_lat": 52.2010,
+                "max_lat": 52.2020,
+                "min_lon": 0.1280,
+                "max_lon": 0.1290
+            },
+            "University Library": {
+                "min_lat": 52.2013,
+                "max_lat": 52.2032,
+                "min_lon": 0.1182,
+                "max_lon": 0.1215
+            },
+            "Squire Law Library": {
+                "min_lat": 52.2051,
+                "max_lat": 52.2065,
+                "min_lon": 0.1149,
+                "max_lon": 0.1173
+            },
+            "Seeley Historical Library": {
+                "min_lat": 52.2035,
+                "max_lat": 52.2055,
+                "min_lon": 0.1200,
+                "max_lon": 0.1220
+            },
+            "Moore Library": {
+                "min_lat": 52.1973,
+                "max_lat": 52.1992,
+                "min_lon": 0.1172,
+                "max_lon": 0.1192
+            },
+            "Betty & Gordon Moore Library": {
+                "min_lat": 52.2050,
+                "max_lat": 52.2070,
+                "min_lon": 0.1205,
+                "max_lon": 0.1225
+            }
         }
 
-        library_name = st.selectbox("Select a library:", list(libraries.keys()))
+
+        
+        library_name = st.selectbox("Select a library:", list(department_libraries_coordinates.keys()))
         st.write(f"You selected: {library_name}")
 
+        location = streamlit_geolocation()
+        st.write(location)
+        print(location["latitude"]) 
+
+        verif = False
+        if department_libraries_coordinates[library_name]["min_lat"] <= location["latitude"] <= department_libraries_coordinates[library_name]["max_lat"] and \
+            department_libraries_coordinates[library_name]["min_lon"] <= location["longitude"] <= department_libraries_coordinates[library_name]["max_lon"]:
+            verif = True
+
+        st.write(verif)
         # Return the coordinates of the selected library
-        return libraries[library_name]
+        return department_libraries_coordinates[library_name]
